b87e47e4ccc609de1333526cddc8043594342e92
Arthur Beyer <abeyer99@uw.edu>
2021-01-19 18:28:24 -0800

    working part A

--- HW1/client.py
+++ HW1/client.py
@@ -2,17 +2,37 @@ import socket
 import struct
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    payload_len = 16
-    p_secret = 0
-    step = 1
-    student_id = 972
-    message_bytes = b'hello world\0'
-    print(len(message_bytes))
+	#### STAGE A ####
+	payload_len = 12
+	p_secret = 0
+	step = 1
+	student_id = 972
+	message_bytes = b'hello world\0'
 
-    message = struct.pack('!hhhis', payload_len, p_secret, step, student_id, message_bytes)
-    print(message)
-    s.sendto(message, ('attu3.cs.washington.edu', 12235))
-    print("Sending data")
-    data, address = s.recvfrom(4096)
-    print("Received message : ", data.decode('utf-8'), "\n\n")
-    s.close()
+	message = struct.pack('!IIHH12s', payload_len, p_secret, step, student_id, message_bytes)
+
+	print("Sending data A")
+	s.sendto(message, ('attu3.cs.washington.edu', 12235))
+	print("Sent data A")
+	data, _ = s.recvfrom(28)
+	payload_len, p_secret, step, student_id, num, len, udp_port, secretA = \
+			struct.unpack('!IIHHIIII', data)
+
+	print(type(udp_port))
+	print("Received message A")
+	print((payload_len, p_secret, step, student_id, num,len,udp_port,secretA))
+
+	message = struct.pack('!I', 0)
+	print("Sending data B")
+	s.sendto(message, ('attu3.cs.washington.edu', udp_port))
+	print("Sent data B")
+
+	#### STAGE B ####
+	for packet_id in range(num):
+		payload = ''
+		payload = bytes(payload.zfill(len), 'utf-8')
+		message = struct.pack('!IIHHI' + str(len) + 's', len+4, secretA, step, student_id, packet_id, payload)
+		print(len(message))
+		s.sendto(message, ('attu3.cs.washington.edu', udp_port))
+
+	s.close()
