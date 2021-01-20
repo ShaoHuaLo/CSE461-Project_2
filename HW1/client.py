91038351b7b6bb08c0df1805f4e819d8b5cf8c59
Arthur Beyer <abeyer99@uw.edu>
2021-01-19 18:53:39 -0800

    merging

--- HW1/client.py
+++ HW1/client.py
@@ -15,24 +15,31 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
 	s.sendto(message, ('attu3.cs.washington.edu', 12235))
 	print("Sent data A")
 	data, _ = s.recvfrom(28)
-	payload_len, p_secret, step, student_id, num, len, udp_port, secretA = \
+	payload_len, p_secret, step2, student_id, num, len, udp_port, secretA = \
 			struct.unpack('!IIHHIIII', data)
 
-	print(type(udp_port))
-	print("Received message A")
-	print((payload_len, p_secret, step, student_id, num,len,udp_port,secretA))
+	# round len up to the nearet number divisible by 4
+	len = int(4 * round(len/4))
 
-	message = struct.pack('!I', 0)
-	print("Sending data B")
-	s.sendto(message, ('attu3.cs.washington.edu', udp_port))
-	print("Sent data B")
+	print("Received message A")
+	print((payload_len, p_secret, step2, student_id, num,len,udp_port,secretA))
 
 	#### STAGE B ####
-	for packet_id in range(num):
-		payload = ''
-		payload = bytes(payload.zfill(len), 'utf-8')
-		message = struct.pack('!IIHHI' + str(len) + 's', len+4, secretA, step, student_id, packet_id, payload)
-		print(len(message))
-		s.sendto(message, ('attu3.cs.washington.edu', udp_port))
+	s.settimeout(0.5)
+	payload = ''
+	payload = bytes(payload.zfill(len), 'utf-8')
+	while packet_id < num:
+		try:
+			message = struct.pack('!IIHHI' + str(len) + 's', len+4, secretA, step, student_id, packet_id, payload)
+			data = struct.unpack('!IIHHI' + str(len) + 's', message)
+			print(data)
+
+			s.sendto(message, ('attu3.cs.washington.edu', udp_port))
+			packet_id = packet_id + 1
+			response, _ = s.recvfrom(16)
+			resp = struct.unpack('!IIHHI', response)
+			print(resp)
+		except socket.timeout:
+			print("Resending packet")
 
 	s.close()
