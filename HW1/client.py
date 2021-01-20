a7c89d02e328f48c07ab26bb3941e1a0d8870d54
Michael <eiza21@uw.edu>
2021-01-19 16:59:37 -0800

    WIP stage a1

--- HW1/client.py
+++ HW1/client.py
@@ -1,14 +1,18 @@
 import socket
 import struct
 
-# addr_info = socket.getaddrinfo('attu2.cs.washington.edu', 12235)
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect(('128.208.1.138', 12235))
-    message = struct.pack('hello world\0')
-    s.send(message)
-    data = s.recv(1024)
-    s.close()
-    # struct.pack() use this for making the header
-    # when writing a strting write b'hello\0'
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    payload_len = 16
+    p_secret = 0
+    step = 1
+    student_id = 972
+    message_bytes = b'hello world\0'
+    print(len(message_bytes))
 
-print('Received', repr(data))
+    message = struct.pack('!hhhis', payload_len, p_secret, step, student_id, message_bytes)
+    print(message)
+    s.sendto(message, ('attu3.cs.washington.edu', 12235))
+    print("Sending data")
+    data, address = s.recvfrom(4096)
+    print("Received message : ", data.decode('utf-8'), "\n\n")
+    s.close()
