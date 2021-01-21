e1adaab19de520b807a725b8126e0b1c1dff86f0
Arthur Beyer <47925992+abeyer99@users.noreply.github.com>
2021-01-21 13:40:18 -0800

    cleaning up code from part 1

--- HW1/client.py
+++ HW1/client.py
@@ -2,6 +2,10 @@ import socket
 import struct
 import math
 
+# don't change these
+STEP = 1
+STUDENT_ID = 972
+
 # round x up to the nearet number divisible by 4
 def roundUpTo4(x):
 	return int(4 * math.ceil(x/4))
@@ -10,36 +14,29 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
 	#### STAGE A ####
 	payload_len = 12
 	p_secret = 0
-	step = 1
-	student_id = 972
 	message_bytes = b'hello world\0'
 
-	message = struct.pack('!IIHH12s', payload_len, p_secret, step, student_id, message_bytes)
+	message = struct.pack('!IIHH12s', payload_len, p_secret, STEP, STUDENT_ID, message_bytes)
 	data = struct.unpack('!IIHH12s', message)
 
 	s.sendto(message, ('attu3.cs.washington.edu', 12235))
 	reponse, _ = s.recvfrom(28)
-	_, _, _, _, num, len, udp_port, secretA = struct.unpack('!IIHHIIII', reponse)
-
-	len_rounded = roundUpTo4(len)
-
+	_, _, _, _, num1, len1, udp_port, secretA = struct.unpack('!IIHHIIII', reponse)
 	print("Secret A: ", secretA)
 
 	#### STAGE B ####
+	len1_rounded = roundUpTo4(len1)
 	s.settimeout(0.5)
-	payload = ''.ljust(len_rounded, '\0')
-	payload = bytes(payload, 'utf-8')
+	payload1 = ''.ljust(len1_rounded, '\0')
+	payload1 = bytes(payload1, 'utf-8')
 	packet_id = 0
-	while packet_id < num:
+	while packet_id < num1:
 		try:
-			message = struct.pack('!IIHHI' + str(len_rounded) + 's', len+4, secretA, step, student_id, packet_id, payload)
-			data = struct.unpack('!IIHHI' + str(len_rounded) + 's', message)
-			# print(data)
+			message = struct.pack('!IIHHI' + str(len1_rounded) + 's', len1+4, secretA, STEP, STUDENT_ID, packet_id, payload1)
 
 			s.sendto(message, ('attu3.cs.washington.edu', udp_port))
-			response, _ = s.recvfrom(16)
+			s.recvfrom(16)
 
-			resp = struct.unpack('!IIHHI', response)
 			print("Received message")
 			packet_id = packet_id + 1
 		except socket.timeout:
@@ -57,12 +54,11 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
 
 	#### STAGE D ####
 	len2_rounded = roundUpTo4(len2)
-	payload = ''.ljust(len2_rounded, c.decode('utf-8'))
-	payload = bytes(payload, 'utf-8')
-	message = struct.pack('!IIHH' + str(len2_rounded) + 's', len2, secretC, step, student_id, payload)
-	data = struct.unpack('!IIHH' + str(len2_rounded) + 's', message)
+	payload2 = ''.ljust(len2_rounded, c.decode('utf-8'))
+	payload2 = bytes(payload2, 'utf-8')
+	message = struct.pack('!IIHH' + str(len2_rounded) + 's', len2, secretC, STEP, STUDENT_ID, payload2)
 
-	for i in range(num2):
+	for _ in range(num2):
 		s.sendall(message)
 
 	response = s.recv(16)
