56ebce1117a708b99a45cec4f95c4323174e3d0c
Arthur Beyer <47925992+abeyer99@users.noreply.github.com>
2021-01-21 13:30:33 -0800

    working part 1

--- HW1/client.py
+++ HW1/client.py
@@ -1,5 +1,10 @@
 import socket
 import struct
+import math
+
+# round x up to the nearet number divisible by 4
+def roundUpTo4(x):
+	return int(4 * math.ceil(x/4))
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
 	#### STAGE A ####
@@ -11,68 +16,57 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
 
 	message = struct.pack('!IIHH12s', payload_len, p_secret, step, student_id, message_bytes)
 	data = struct.unpack('!IIHH12s', message)
-	print(data)
 
-	print("Sending data A")
 	s.sendto(message, ('attu3.cs.washington.edu', 12235))
-	print("Sent data A")
-	data, _ = s.recvfrom(28)
-	payload_len, p_secret, step2, student_id, num, len, udp_port, secretA = \
-			struct.unpack('!IIHHIIII', data)
+	reponse, _ = s.recvfrom(28)
+	_, _, _, _, num, len, udp_port, secretA = struct.unpack('!IIHHIIII', reponse)
 
-	# round len up to the nearet number divisible by 4
-	len = int(4 * round(len/4))
+	len_rounded = roundUpTo4(len)
 
-	print("Received message A")
-	print((payload_len, p_secret, step2, student_id, num,len,udp_port,secretA))
+	print("Secret A: ", secretA)
 
 	#### STAGE B ####
-	s.settimeout(.5)
-	payload = ''.ljust(len, '\0')
+	s.settimeout(0.5)
+	payload = ''.ljust(len_rounded, '\0')
 	payload = bytes(payload, 'utf-8')
 	packet_id = 0
 	while packet_id < num:
 		try:
-			message = struct.pack('!IIHHI' + str(len) + 's', len+4, secretA, step, student_id, packet_id, payload)
-			data = struct.unpack('!IIHHI' + str(len) + 's', message)
+			message = struct.pack('!IIHHI' + str(len_rounded) + 's', len+4, secretA, step, student_id, packet_id, payload)
+			data = struct.unpack('!IIHHI' + str(len_rounded) + 's', message)
 			# print(data)
 
 			s.sendto(message, ('attu3.cs.washington.edu', udp_port))
 			response, _ = s.recvfrom(16)
 
-			packet_id = packet_id + 1
 			resp = struct.unpack('!IIHHI', response)
 			print("Received message")
+			packet_id = packet_id + 1
 		except socket.timeout:
 			print("Resending packet")
 	response, _ = s.recvfrom(20)
 	_, _, _, _, tcp_port, secretB = struct.unpack('!IIHHII', response)
-	print("tcp port: ", tcp_port)
-	print("Secret B:", secretB)
+	print("Secret B: ", secretB)
 
-	### STAGE C ###
+	#### STAGE C ####
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect(('attu3.cs.washington.edu', tcp_port))
-	response = s.recv(25)
-	payload_len, p_secret, step2, student_id, num2, len2, secretC, c = struct.unpack('!IIHHIIIc', response)
-	print(struct.unpack('!IIHHIIIc', response))
+	response = s.recv(28)
+	_, _, _, _, num2, len2, secretC, c, _ = struct.unpack('!IIHHIIIc3s', response)
 	print("Secret C: ", secretC)
 
-	## STAGE D ##
-	len2 = int(4 * round(len2 / 4))
-	# Had issues passing in c into ljust this is a workaround but there must be a better way
-	s_char = struct.pack('s', c)
-	payload = ''.ljust(len2, chr(s_char[0]))
+	#### STAGE D ####
+	len2_rounded = roundUpTo4(len2)
+	payload = ''.ljust(len2_rounded, c.decode('utf-8'))
 	payload = bytes(payload, 'utf-8')
-	for packet_id in range(num2):
-		message = struct.pack('!IIHHI' + str(len2) + 's', len2 + 4, secretC, step, student_id, packet_id, payload)
-		data = struct.unpack('!IIHHI' + str(len2) + 's', message)
-		print(data)
-		# since this is tcp I am not sure if we need to worry about timeout.
+	message = struct.pack('!IIHH' + str(len2_rounded) + 's', len2, secretC, step, student_id, payload)
+	data = struct.unpack('!IIHH' + str(len2_rounded) + 's', message)
+
+	for i in range(num2):
 		s.sendall(message)
-	# Keep running into an issue that there are too many values to unpack
-	response, _ = s.recv(16)
-	payload_len, p_secret, step, student_id, secretD = struct.unpack('!IIHHI', response)
+
+	response = s.recv(16)
+	_, _, _, _, secretD = struct.unpack('!IIHHI', response)
 	print("Secret D: ", secretD)
 
 	s.close()
