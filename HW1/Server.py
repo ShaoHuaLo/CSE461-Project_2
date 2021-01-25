bfd205f9201b554475ce7609580fea1683258f96
Michael <eiza21@uw.edu>
2021-01-24 23:45:45 -0800

    Working Client/Server

---
+++ HW1/Server.py
@@ -0,0 +1,262 @@
+import struct
+import socket
+import random
+import math
+import _thread
+
+STEP = 2
+STUDENT_ID = 972
+
+# SERVER_NAME = "attu2.cs.washington.edu"
+SERVER_NAME = "localhost" # testing
+PORT_START = 12235
+
+HEADER_LEN = 12
+BUFFER_SIZE = 1024
+CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+def roundUpTo4(x):
+    """
+    :param x: length of a payload
+    :return a new length which is divisible by 4
+    """
+    return int(4 * math.ceil(x / 4))
+
+
+def get_header(payload_len, psecret, step = STEP, id = STUDENT_ID):
+    """
+
+    :param payload_len: length of payload which excludes the padding for byte-align
+    :param psecret: secret of last step
+    :param step: constant, value 1 for the client and 2 for the server respectively
+    :param id: student_id
+    :return: Object of bytes of length 12 bytes long.
+    """
+    header = struct.pack('!IIHH', payload_len, psecret, step, id)
+    return header
+
+
+def is_valid_packet_size(packet):
+    """
+    :param: a packet(type of python bytes) received through the newtworking
+    :return: Boolean True if the packet'size is divisible by 4, False otherwise
+    """
+    if len(packet) % 4 != 0:
+        print("packet size: ", len(packet), "which is not divisible by 4")
+        return False
+    else:
+        return True
+
+
+def resolve_packet(packet_rec):
+    """
+    This utility function will extract the data we need from the original packet
+    :param packet_rec: A packet received through the networking
+    :return: A tuple of (header, payload) of which the :header: is of fixed 12-bytes length
+            and the :payload: is only the non-padding part of the original payload
+    """
+
+    header = packet_rec[:12]
+    payload_len, psecret, step, id = struct.unpack('!2I2H', header)
+    payload_with_padding = packet_rec[12:]
+    payload = payload_with_padding[:payload_len]
+    return header, payload
+
+
+def is_valid_packet_a(header, payload):
+    """
+    Verify given packet are valid or not according to each step's context
+    :param header: a packet (of type python bytes)
+    :param payload: boolean True if it's valid packet, False otherwise.
+    :return:
+    """
+
+    payload_len, psecret, step, id = struct.unpack('!IIHH', header)
+    if psecret != 0 or step != 1:
+        print("expected (psecret, step): (0, 1)", "vs. current (", psecret, ", ", step, ")")
+        return False
+    if payload_len != len(b'hello world\0'):
+        return False
+    if payload != b'hello world\0':
+        print("expected payload: ", b'hello world\0', "vs. current: ", payload)
+        return False
+    return True
+
+
+def is_valid_packet_b(header, payload, secretA, len_of_zero, id):
+    payload_len, psecret, step, _ = struct.unpack('!IIHH', header)
+    if psecret != secretA:
+        print("secret not correct")
+        return False
+
+    packet_id, zeros = struct.unpack('! I {}s'.format(len_of_zero), payload)
+    if packet_id != id or step != 1:
+        print("id is wrong", "actual: ", packet_id, " expected: ", id)
+        print("step is wrong")
+        return False
+
+    if len(zeros) != len_of_zero:
+        print("actual len: ", len(zeros), " expected len: ", len_of_zero)
+        return False
+
+    if zeros[4:] != (b'\0' * len_of_zero)[4:]:
+        print("actual len: ", len(zeros), " expected len: ", len_of_zero)
+        print("actual: ", zeros, "expected: ", b'\0' * len_of_zero)
+        return False
+    return True
+
+
+def is_valid_packet_d(header, payload, secretC, c):
+    payload_len, psecret, step, id = struct.unpack('!IIHH', header)
+    if psecret != secretC or step != 1:
+        return False
+    if payload != c*payload_len:
+        return False
+    return True
+
+
+def response_to_client_a(addr_client):
+    num, len0, udp_port, secretA = random.randint(0, 10), random.randint(0, 10),\
+                                   random.randint(49152, 65535), random.randint(0, 50)
+    packet_reply = struct.pack('! 12s 4I', get_header(16, 0), num, len0, udp_port, secretA)
+    print("sending secretA: ", secretA, " to-- ", addr_client)
+    my_UDP_socket.sendto(packet_reply, addr_client)
+    return num, len0, udp_port, secretA
+
+
+def response_to_client_b(socket_b, addr_client, psecret):
+    tcp_port, secretB = random.randint(49152, 65535), random.randint(0, 1000)
+    packet_to_send = struct.pack('! 12s I I', get_header(8, psecret), tcp_port, secretB)
+    print("sending secretB: ", secretB, " to-- ", addr_client)
+    socket_b.sendto(packet_to_send, addr_client)
+    return tcp_port, secretB
+
+
+def response_to_client_c(conn, psecret):
+    num2, len2, secretC, c = random.randint(0, 50), \
+                             random.randint(0, 50),\
+                             random.randint(0, 50), \
+                             bytes(CHARS[random.randint(0, 51)], 'utf-8')
+    packet_to_send = struct.pack('! 12s I I I c 3s', get_header(16, psecret), num2, len2, secretC, c, b'\0\0\0')
+    print("sending secretC: ", secretC, " to-- ", addr_client)
+    conn.sendall(packet_to_send)
+    return num2, len2, secretC, c
+
+
+def response_to_client_d(conn, psecret):
+    secretD = random.randint(0, 50)
+    packet_to_send = struct.pack('! 12s I', get_header(4, psecret), secretD)
+    print("sending secretD: ", secretD, " to -- ", addr_client)
+    conn.sendall(packet_to_send)
+
+
+def send_awk_packet(socket_b, addr_client, psecret):
+    awk_id = random.randint(0, 1000)
+    awk_pack = struct.pack('! 12s I', get_header(1, psecret), awk_id)
+    socket_b.sendto(awk_pack, addr_client)
+
+
+def connect_to_TCP(tcp_port):
+    tcp_master_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_master_socket.bind(("localhost", tcp_port))
+    tcp_master_socket.listen()
+    conn, addr_client = tcp_master_socket.accept()
+    conn.settimeout(3)
+    return conn, addr_client
+
+
+def threaded(packet_recv, addr_client, sock):
+    # step a
+    # verify header & payload
+    header, payload = resolve_packet(packet_recv)
+    if not is_valid_packet_size(payload) or not is_valid_packet_a(header, payload):
+        return
+    num, len0, udp_port, secretA = response_to_client_a(addr_client)
+
+    # -----------------------------------------------------------------------#
+
+    # step b
+    socket_b = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socket_b.bind((SERVER_NAME, udp_port))
+
+    id = 0
+    while id < num:
+        packet_recv, addr_client = socket_b.recvfrom(BUFFER_SIZE)
+        if not packet_recv:
+            print("data stream all sent: transmission over...") # for debugging
+            break
+
+        # verify header & payload
+        if not is_valid_packet_size(packet_recv):
+            print("row165: size not divisible by 4") # for debugging
+            return
+
+        header, payload = resolve_packet(packet_recv)
+        if not is_valid_packet_b(header, payload, secretA, len0, id):
+            print("row171: header or payload is not correct") # for debugging
+            return
+
+        if random.randint(0, 1000) < 200:
+            continue
+
+        send_awk_packet(socket_b, addr_client, secretA)
+        id += 1
+
+    # one more check if the number of packets recved is correct
+    if id != num:
+        print("row188: something went wrong, total packets recv didnt match")
+        return
+
+    tcp_port, secretB = response_to_client_b(socket_b, addr_client, secretA)
+
+    # -----------------------------------------------------------------------#
+
+    # step c1
+    conn, addr_client = connect_to_TCP(tcp_port)
+    # step c2
+    try:
+        num2, len2, secretC, c = response_to_client_c(conn, secretB)
+
+        # -----------------------------------------------------------------------#
+        # d1
+        count = 0
+        while count < num2:
+            packet_recv = conn.recv(12 + roundUpTo4(len2))
+            # if transmission is done
+            if not packet_recv:
+                break
+            # verify header & payload
+            header, payload = resolve_packet(packet_recv)
+            if not is_valid_packet_size(packet_recv):
+                print("packet size is not divisible by 4... terminating the loop")
+                conn.close()
+                return
+            if not is_valid_packet_d(header, payload, secretC, c):
+                print("packet from step-d is not valid... ending the loop")
+                conn.close()
+                return
+            # print("NO-", count," packet recved") # for debugging
+            count += 1
+
+        # one more check
+        if count != num2:
+            print("something went wrong, packet might be missing")
+            conn.close()
+            return
+
+        response_to_client_d(conn, secretC)
+    except socket.timeout:
+        print("3s timeout, reconnecting again")
+    finally:
+        conn.close()
+
+
+if __name__ == "__main__":
+    my_UDP_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    my_UDP_socket.bind((SERVER_NAME, PORT_START))
+    while True:
+        # -----------------------------------------------------------------------#
+        # UDP socket
+        packet_recv, addr_client = my_UDP_socket.recvfrom(BUFFER_SIZE)
+        _thread.start_new_thread(threaded, (packet_recv, addr_client, my_UDP_socket))
--- HW1/client.py
+++ HW1/client.py
@@ -1,78 +1,132 @@
 import socket
+import math
 import struct
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-	#### STAGE A ####
-	payload_len = 12
-	p_secret = 0
-	step = 1
-	student_id = 972
-	message_bytes = b'hello world\0'
+STEP = 1
+STUDENT_ID = 972
 
-	message = struct.pack('!IIHH12s', payload_len, p_secret, step, student_id, message_bytes)
-	data = struct.unpack('!IIHH12s', message)
-	print(data)
+# SERVER_NAME = "attu2.cs.washington.edu"
+SERVER_NAME = "localhost" # for testing
+PORT = 12235
+HEADER_LEN = 12
+BUFFER_SIZE = 1024
 
-	print("Sending data A")
-	s.sendto(message, ('attu3.cs.washington.edu', 12235))
-	print("Sent data A")
-	data, _ = s.recvfrom(28)
-	payload_len, p_secret, step2, student_id, num, len, udp_port, secretA = \
-			struct.unpack('!IIHHIIII', data)
 
-	# round len up to the nearet number divisible by 4
-	len = int(4 * round(len/4))
+# utility function : round up payload len to be divisible by 4
+def roundUpTo4(x):
+    return int(4 * math.ceil(x / 4))
 
-	print("Received message A")
-	print((payload_len, p_secret, step2, student_id, num,len,udp_port,secretA))
 
-	#### STAGE B ####
-	s.settimeout(.5)
-	payload = ''.ljust(len, '\0')
-	payload = bytes(payload, 'utf-8')
-	packet_id = 0
-	while packet_id < num:
-		try:
-			message = struct.pack('!IIHHI' + str(len) + 's', len+4, secretA, step, student_id, packet_id, payload)
-			data = struct.unpack('!IIHHI' + str(len) + 's', message)
-			# print(data)
+# utility function : return header (bytes)
+def get_header(payload_len, psecret, step=STEP, id=STUDENT_ID):
+    header = struct.pack('!IIHH', payload_len, psecret, step, id)
+    return header
 
-			s.sendto(message, ('attu3.cs.washington.edu', udp_port))
-			response, _ = s.recvfrom(16)
 
-			packet_id = packet_id + 1
-			resp = struct.unpack('!IIHHI', response)
-			print("Received message")
-		except socket.timeout:
-			print("Resending packet")
-	response, _ = s.recvfrom(20)
-	_, _, _, _, tcp_port, secretB = struct.unpack('!IIHHII', response)
-	print("tcp port: ", tcp_port)
-	print("Secret B:", secretB)
+def resolve_packet(packet_rec):
+    header = packet_rec[:12]
+    payload = packet_rec[12:]
+    return header, payload
 
-	### STAGE C ###
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect(('attu3.cs.washington.edu', tcp_port))
-	response = s.recv(25)
-	payload_len, p_secret, step2, student_id, num2, len2, secretC, c = struct.unpack('!IIHHIIIc', response)
-	print(struct.unpack('!IIHHIIIc', response))
-	print("Secret C: ", secretC)
 
-	## STAGE D ##
-	len2 = int(4 * round(len2 / 4))
-	# Had issues passing in c into ljust this is a workaround but there must be a better way
-	s_char = struct.pack('s', c)
-	payload = ''.ljust(len2, chr(s_char[0]))
-	payload = bytes(payload, 'utf-8')
-	for packet_id in range(num2):
-		message = struct.pack('!IIHHI' + str(len2) + 's', len2 + 4, secretC, step, student_id, packet_id, payload)
-		data = struct.unpack('!IIHHI' + str(len2) + 's', message)
-		print(data)
-		# since this is tcp I am not sure if we need to worry about timeout.
-		s.sendall(message)
-	# Keep running into an issue that there are too many values to unpack
-	response, _ = s.recv(16)
-	payload_len, p_secret, step, student_id, secretD = struct.unpack('!IIHHI', response)
-	print("Secret D: ", secretD)
+def byte_align(payload_before_padding):
+    n = len(payload_before_padding)
+    if n % 4 == 0:
+        return payload_before_padding, n, n
+    new_n = roundUpTo4(n)
+    diff = new_n - n
+    new_pay_load = payload_before_padding + b'\0' * diff
+    return new_pay_load, n, new_n
 
-	s.close()
+
+def stage_A():
+    my_UDP_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    payload = b'hello world\0'
+    payload_len = len(payload)
+
+    # Step a1
+    # prepare header and packet
+    header = get_header(payload_len, 0)
+    packet_to_send = struct.pack('! 12s {}s'.format(payload_len), header, payload)
+    my_UDP_socket.sendto(packet_to_send, (SERVER_NAME, PORT))
+
+    # Step a2
+    packet_recv = my_UDP_socket.recv(BUFFER_SIZE)
+    # last 4 number are what we want
+    header, num, len1, udp_port, secretA  = struct.unpack('! 12s 4I', packet_recv)
+    print("Secret A: ", secretA)
+    return num, len1, udp_port, secretA
+
+
+def stage_B(num, len1, udp_port, secretA):
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    my_socket.settimeout(0.5)
+    header = get_header(len1+4, secretA)
+    zeros = b'\0' * len1
+    # Step b1
+    packet_id = 0
+    while packet_id < num:
+        payload_before_padding = int.to_bytes(packet_id, 4, 'big', signed=False) + zeros
+        payload_after_padding, before_len, after_len = byte_align(payload_before_padding)
+        packet_to_send = struct.pack('!12s {}s'.format(after_len), header, payload_after_padding)
+        try:
+            my_socket.sendto(packet_to_send, (SERVER_NAME, udp_port))
+            awk = my_socket.recv(BUFFER_SIZE)
+
+            # check if we need to resend this pack_id
+            if not awk:
+                print("didn't receive awk, resending")
+                continue
+            packet_id += 1
+
+        except socket.timeout:
+            print("0.5s timeout in b1, retrying")
+            continue;
+
+    # Step b2
+    try:
+        packet_recv = my_socket.recv(BUFFER_SIZE)
+        _, tcp_port, secretB = struct.unpack('! 12sII', packet_recv)
+        print("Secret B: ", secretB)
+        return tcp_port, secretB
+    except socket.timeout:
+        print("timeout in b2")
+        return -1, -1
+
+
+def stage_C_D(tcp_port, secretB):
+    # if stage B failed exit the program
+    if tcp_port == -1 and secretB == -1:
+        return
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as my_tcp_socket:
+        # step c1
+        print(tcp_port)
+        my_tcp_socket.connect((SERVER_NAME, tcp_port))
+        # step c2
+        packet_recv = my_tcp_socket.recv(BUFFER_SIZE)
+        _, num2, len2, secretC, c, _ = struct.unpack('! 12s 3I c 3s', packet_recv)
+        print("Secret C:", secretC)
+
+        # step d1
+        payload_before_padding = c * len2
+        payload_after_padding, before_len, after_len = byte_align(payload_before_padding)
+        header = get_header(len2, secretC)
+        packet_to_send = struct.pack('! 12s {}s'.format(after_len), header, payload_after_padding)
+
+        for i in range(num2):
+            my_tcp_socket.sendall(packet_to_send)
+
+        # step d2
+        packet_recv = my_tcp_socket.recv(BUFFER_SIZE)
+        _, secretD = struct.unpack('! 12s I', packet_recv)
+        print("secret D: ", secretD)
+
+
+def main():
+    num, len1, udp_port, secretA = stage_A()
+    tcp_port, secretB = stage_B(num, len1, udp_port, secretA)
+    stage_C_D(tcp_port, secretB)
+
+
+if __name__ == "__main__":
+    main()
